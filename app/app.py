bfb378388290aca808fc9bf9b70fdd529966d5ec
shin <shinichi.hatano@ctc-g.co.jp>
2016-11-30 10:06:46 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -62,7 +62,8 @@ def callback():
 def download_imagemap(size):
     filename = const.POKER_IMAGE_FILENAME.replace('{$size}', size)
     app.logger.info('requested imagemap file :' + filename)
-    return send_from_directory("static/planning_poker/", filename)
+    return send_from_directory(os.path.join(app.root_path, 'static/planning_poker/'),
+            filename)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
